34397e7ff6d5334cd49bd6b7f8181126d13b04a8
way2muchnoise <jens.baetens@live.be>
2014-12-22 22:02:03 +0100

    adds some test data
    now @synkrone has to do the rest

--- src/main/java/customenchanting/enchant/EnchantmentRegistry.java
+++ src/main/java/customenchanting/enchant/EnchantmentRegistry.java
@@ -2,6 +2,7 @@ package customenchanting.enchant;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentData;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -39,6 +40,20 @@ public class EnchantmentRegistry
 
     public static void init()
     {
+        addEnchant(new ItemStack(Items.leather_chestplate, 1), new EnchantmentData(Enchantment.protection, 1));
+        addEnchant(new ItemStack(Items.iron_chestplate, 1), new EnchantmentData(Enchantment.protection, 2));
+        addEnchant(new ItemStack(Items.golden_chestplate, 1), new EnchantmentData(Enchantment.protection, 3));
         addEnchant(new ItemStack(Items.diamond_chestplate, 1), new EnchantmentData(Enchantment.protection, 4));
+        addEnchant(new ItemStack(Items.feather, 8), new EnchantmentData(Enchantment.featherFalling, 1));
+        addEnchant(new ItemStack(Items.feather, 16), new EnchantmentData(Enchantment.featherFalling, 2));
+        addEnchant(new ItemStack(Items.feather, 32), new EnchantmentData(Enchantment.featherFalling, 3));
+        addEnchant(new ItemStack(Items.feather, 64), new EnchantmentData(Enchantment.featherFalling, 4));
+        addEnchant(new ItemStack(Blocks.tnt, 1), new EnchantmentData(Enchantment.blastProtection, 1));
+        addEnchant(new ItemStack(Blocks.tnt, 4), new EnchantmentData(Enchantment.blastProtection, 2));
+        addEnchant(new ItemStack(Blocks.tnt, 8), new EnchantmentData(Enchantment.blastProtection, 3));
+        addEnchant(new ItemStack(Blocks.tnt, 12), new EnchantmentData(Enchantment.blastProtection, 4));
+        addEnchant(new ItemStack(Blocks.cactus, 16), new EnchantmentData(Enchantment.thorns, 1));
+        addEnchant(new ItemStack(Blocks.cactus, 32), new EnchantmentData(Enchantment.thorns, 2));
+        addEnchant(new ItemStack(Blocks.cactus, 64), new EnchantmentData(Enchantment.thorns, 3));
     }
 }
